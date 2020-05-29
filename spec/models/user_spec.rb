130ae74b561efafe24b57d8febc8d38c1c57f661
silverskg <chokichoki_gm@i.softbank.jp>
2020-05-29 21:50:21 +0900

    rspec-サンプルテスト実行

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "名前の表示" do
+
+    #パラメータを定義する
+    let(:params){{name: "サトル"}}
+    
+    it "年齢1:ユーザーの名前が「〜ちゃん」と取得できること" do
+      params.merge!(age: 1)
+      user = User.new(params)
+      expect(user.disp_name).to eq "サトルちゃん"
+    end
+    it "年齢15:ユーザーの名前が「〜君」と取得できること" do
+      params.merge!(age: 15)
+      user = User.new(params)
+      expect(user.disp_name).to eq "サトル君"
+    end
+    it "年齢20:ユーザーの名前が「〜さん」と取得できること" do
+      params.merge!(age: 20)
+      user = User.new(params)
+      expect(user.disp_name).to eq "サトルさん"
+    end
+    it "年齢-1:不正な値ですと取得できること" do
+      params.merge!(age: -1)
+      user = User.new(params)
+      expect(user.disp_name).to eq "不正な値です"
+    end
+  end
+end
